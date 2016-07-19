81f4d30f90faf7dc5a78055bdab9463bad1c573b
Andreas Tempsch <andreas.tempsch@geizhals.at>
2016-07-19 13:47:29 +0200

    convert sourcefile to latin1 before parsing
    Signed-off-by: Andreas Tempsch <andreas.tempsch@geizhals.at>

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+var encoding = require("encoding");
+
 module.exports = function(source) {
 	this.cacheable && this.cacheable();
-	var value = typeof source === "string" ? JSON.parse(source) : source;
+	var value = typeof source === "string" ? JSON.parse(encoding.convert(source, 'Latin_1')) : source;
 	this.value = [value];
 	return "module.exports = " + JSON.stringify(value, undefined, "\t") + ";";
 }
